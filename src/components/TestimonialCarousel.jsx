ce302996c63a23951b489bf1fda0fbeeea2ec8eb
soifuturegit <soifuture.git@gmail.com>
2025-11-24 02:08:35 +0600

    Updated TestimonialCarousel.jsx

--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -65,7 +65,7 @@ export default function TestimonialCarousel({ testimonials }) {
                 className="relative flex-shrink-0 pt-16"
                 style={{ width: `${100 / testimonials.length}%` }}
               >
-                <div className="absolute left-1/2 transform -translate-x-1/2 -top-0 z-20">
+                <div className="absolute left-1/2 transform -translate-x-1/2 top-0 z-20">
                   <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-white shadow-xl">
                     <img
                       src={testimonial.image || 'https://via.placeholder.com/160'}
@@ -76,8 +76,8 @@ export default function TestimonialCarousel({ testimonials }) {
                 </div>
 
                 <div
-                  className="rounded-3xl shadow-lg p-6 lg:p-8 relative flex flex-col pt-20"
-                  style={{ backgroundColor: '#E9ECFE', minHeight: '320px' }}
+                  className="rounded-3xl shadow-lg p-6 lg:p-8 relative flex flex-col"
+                  style={{ backgroundColor: '#E9ECFE', minHeight: '320px', paddingTop: '88px' }}
                 >
                   <div className="absolute left-6 top-6">
                     <div className="w-14 h-14 rounded-full flex items-center justify-center" style={{ backgroundColor: '#9B8FD9' }}>
